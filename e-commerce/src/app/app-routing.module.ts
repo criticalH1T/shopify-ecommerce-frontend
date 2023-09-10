cce8904c85ba8b84ff02932fa89103dd9f9603d4
Conesvkie <conevskie@gmail.com>
2023-09-10 03:59:57 +0300

    header stuff

--- e-commerce/src/app/app-routing.module.ts
+++ e-commerce/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ProductLandingPageComponent} from "./product-landing-page/product-landing-page.component";
+import {ProductListComponent} from "./product-list/product-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ProductLandingPageComponent},
+  { path: 'products/:id', component: ProductListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- e-commerce/src/app/app.module.ts
+++ e-commerce/src/app/app.module.ts
@@ -6,17 +6,25 @@ import { AppComponent } from './app.component';
 import { ProductLandingPageComponent } from './product-landing-page/product-landing-page.component';
 import { LoginComponent } from './login/login.component';
 import { CheckoutComponent } from './checkout/checkout.component';
+import { HeaderComponent } from './header/header.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import {NgOptimizedImage} from "@angular/common";
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductLandingPageComponent,
     LoginComponent,
-    CheckoutComponent
+    CheckoutComponent,
+    HeaderComponent,
+    ProductListComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgOptimizedImage
   ],
   providers: [],
   bootstrap: [AppComponent]
